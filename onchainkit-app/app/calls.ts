d41a3b709b0011a56e10b3d53edeec848df04726
Sandile88 <sandilep.mremi@gmail.com>
2025-07-13 14:31:19 +0200

    interacted with deployed contract and change frontend

---
+++ onchainkit-app/app/calls.ts
@@ -0,0 +1,19 @@
+const counterContractAddress = '0xBAa0CA2d63bfA94a95e6372391bc8247e3F00d32' as `0x${string}`;
+const counterContractAbi = [
+  {
+    type: 'function',
+    name: 'increment',
+    inputs: [],
+    outputs: [],
+    stateMutability: 'nonpayable',
+  },
+] as const;
+
+export const calls = [
+  {
+    address: counterContractAddress,
+    abi: counterContractAbi,
+    functionName: 'increment',
+    args: [],
+  },
+];
