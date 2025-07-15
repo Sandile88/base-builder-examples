5f7e5a14380ae7ffc3af6ceba3593d2d2bc862bb
Sandile88 <sandilep.mremi@gmail.com>
2025-07-15 22:56:52 +0200

    createe Guestbook contract, yet to add more funcs

--- guestbook-dapp/contracts/src/Counter.sol
+++ guestbook-dapp/contracts/src/Counter.sol
@@ -1,14 +1,56 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+contract Guestbook {
+    struct Message{
+        address author;
+        string title;
+        string text;
     }
 
-    function increment() public {
-        number++;
+    mapping (uint => Message) public messages;
+
+    address public owner;
+
+    // keeps track of how many messages exist and returns the latest one
+    uint public messageCount = 0;
+
+    event MessageWritten(uint id, address indexed author);
+    event MessageEdited(uint id, address indexed editor);
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+    // anyone can post a message
+    function writeMessage(string memory _title, string memory _text) public checkEmptyString(_title, _text) {
+        messages[messageCount] = Message(msg.sender, _title, _text);
+        messageCount++;
+
+        emit MessageWritten(messageCount - 1, msg.sender);
+    }
+
+
+    function editMessage(uint _id, string memory _title, string memory _text) public checkEmptyString(_title, _text){
+        Message storage m = messages[_id];
+        require(msg.sender == m.author, "Only author can edit");
+
+        if (bytes(_title).length > 0) {
+            m.title = _title;
+        }
+
+        if (bytes(_text).length > 0) {
+            m.text = _text;
+        }
+
+        emit MessageEdited(messageCount -1, msg.sender);
+    }
+
+
+    // checking if the input string are empty
+    // ensure at least one field is not empty
+    modifier checkEmptyString(string memory _title, string memory _text) {
+        require(bytes(_title).length > 0 || bytes(_text).length > 0 , "Both title and text are empty");
+        _;
     }
 }
